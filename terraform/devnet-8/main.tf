aa8b31f01ab465286de8355b5e9db01bd20e85a5
Barnabas Busa <busa.barnabas@gmail.com>
2023-07-19 14:35:31 +0200

    bookworm is the best kind of worm

--- terraform/devnet-8/main.tf
+++ terraform/devnet-8/main.tf
@@ -195,7 +195,7 @@ variable "digitalocean_vm_groups" {
 locals {
   digitalocean_default_region = "ams3"
   digitalocean_default_size   = "s-2vcpu-4gb-amd"
-  digitalocean_default_image  = "debian-11-x64"
+  digitalocean_default_image  = "debian-12-x64"
   digitalocean_global_tags = [
     "Owner:Devops",
     "EthNetwork:${var.ethereum_network}"
