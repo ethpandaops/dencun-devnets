9158388ca7fda15a6988805422fb991e1deafe7c
Barnabas Busa <busa.barnabas@gmail.com>
2023-10-10 11:23:15 +0200

    feat: add devnet 10

---
+++ terraform/devnet-10/.terraform.lock.hcl
@@ -0,0 +1,68 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/cloudflare/cloudflare" {
+  version     = "3.35.0"
+  constraints = "~> 3.0"
+  hashes = [
+    "h1:pn9uUSAuIE8XgqJuZ9fOs98bRN9qw4o0JHFgmwtbMyI=",
+    "zh:13aabc00fee823422831bcc870227650cc765fc4c9622074d24d6d62a4ac0e37",
+    "zh:1544405f0ea6b388dad7eb25c434427b2682417396da9186e1b33551e6b4adff",
+    "zh:5d58394cb8e71bd4bf6ef0135f1ca6a4ad2cec937f3731b224125eb34ee059f7",
+    "zh:648596ed545ed01ae757d5a0b37c20e8050cfb51d42e9a2c82fcc94d883ff11d",
+    "zh:68d75e14eef4f073faa975ed6baf4db7e0e1f2fc61a4e54fd95325df42793810",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:9916cc626fef57428c4c60db7897b34068c65639b68482e94f62d97d773d64bc",
+    "zh:9c8c9f369eb30e7360a0ebd7918e4846ca4d5bca430b861fdbde7522a3146459",
+    "zh:a40e244688bbcb6f1a771e6ea89fb0b0b7bb53be3fab718abc66b3593e0f8133",
+    "zh:cc5a6191aa8713275550ff2b6adda6e6d56e4780c9cbe3d1da1dc23ea893bfff",
+    "zh:d1dd435780e8c7e79bff26b46a76df0e123971849355ad17877d1e24dc5953c3",
+    "zh:d751fc72f2833f2bdb897fa89de2bb5b6efbad1e648896642f0e6fe5cde789c8",
+    "zh:dfc4c90b3605ec1bb7cc7a9f1fb1b67235578bdd6b9be78e7b3516b55d0422db",
+    "zh:e6101a80fe24e2df3ab60152458ff1666a4a1befc87c62e459a219cdbb53e6df",
+    "zh:e9bcf26c44dd231f74703b6a6717470021a3ba7e1d7531dcf7287a6441300e27",
+  ]
+}
+
+provider "registry.terraform.io/digitalocean/digitalocean" {
+  version     = "2.28.1"
+  constraints = "~> 2.28"
+  hashes = [
+    "h1:vQLbGapH57do82BFLqI9XLyLIy/K/ucSVXOlbNFICkk=",
+    "zh:2e22294110ddfc4cd8c51e342f56788175d02b2bb311f1834f3c144a80dc30dc",
+    "zh:59641f0c7b10befced370008a3178670ee1103fcb504a9b71f90d6f697738fc2",
+    "zh:5d4c48701dbf3316cc149a01e44bed8cacb4426d4981a415ca1149a26af608af",
+    "zh:5fc27f1948669378d5f1d0cd0352fed92a3516738cb4bdeff026ea9242e364b0",
+    "zh:673bb803646c359809db39ccead45c1ea5ab12b47dd4dd14a576fe5c6300e386",
+    "zh:67e212f02ac0acdfc9d448299acefc3e4ea6aaad9a229f7f97e9064e1512a33f",
+    "zh:6dcd108fb68ce1b1cbfe9d4c4df0f0f4c3c09bd6154724df9bd33e1630cd6e0b",
+    "zh:757e4b2f3c728a6b781521e8f7e47ed8bd7bf189f5e273d953a0d849463d9af5",
+    "zh:7ae1a6cb34c45a00f84090ad2dcc7eac23fd748a026801bfe19e60a96893882f",
+    "zh:7fecfea5b2b2e79ee1c49b824995ed5232e7f538fdfe33aa984a1bda90bf1587",
+    "zh:8e5671bd7cbc2e45e3e9e4ec803e8d9b671f687c66963f52bf1bf58cb7b05819",
+    "zh:a6a5b504c95eff173e2ac017523a97e3300d93fa887465b0278c2633dd0cc608",
+    "zh:cd273e7c690e6758761f583a35f76d0e86259ccc842be137931e4cf9d8c3d1cc",
+    "zh:e815c665ed5c32057d1f4dc0288b6387ba38b2a5bf5ef0c36987810a141d3b39",
+    "zh:ebac3d11f2e968f88f95e3f277b547f505ecd7150df0fd763087499b251bca9e",
+    "zh:fb970aa84783edc03ea4ec53d2b896721fbab08b4b764a284e3d7eb49bfa046e",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.4.0"
+  hashes = [
+    "h1:ZUEYUmm2t4vxwzxy1BvN1wL6SDWrDxfH7pxtzX8c6d0=",
+    "zh:53604cd29cb92538668fe09565c739358dc53ca56f9f11312b9d7de81e48fab9",
+    "zh:66a46e9c508716a1c98efbf793092f03d50049fa4a83cd6b2251e9a06aca2acf",
+    "zh:70a6f6a852dd83768d0778ce9817d81d4b3f073fab8fa570bff92dcb0824f732",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:82a803f2f484c8b766e2e9c32343e9c89b91997b9f8d2697f9f3837f62926b35",
+    "zh:9708a4e40d6cc4b8afd1352e5186e6e1502f6ae599867c120967aebe9d90ed04",
+    "zh:973f65ce0d67c585f4ec250c1e634c9b22d9c4288b484ee2a871d7fa1e317406",
+    "zh:c8fa0f98f9316e4cfef082aa9b785ba16e36ff754d6aba8b456dab9500e671c6",
+    "zh:cfa5342a5f5188b20db246c73ac823918c189468e1382cb3c48a9c0c08fc5bf7",
+    "zh:e0e2b477c7e899c63b06b38cd8684a893d834d6d0b5e9b033cedc06dd7ffe9e2",
+    "zh:f62d7d05ea1ee566f732505200ab38d94315a4add27947a60afa29860822d3fc",
+    "zh:fa7ce69dde358e172bd719014ad637634bbdabc49363104f4fca759b4b73f2ce",
+  ]
+}
---
+++ terraform/devnet-10/nodes.tf
@@ -0,0 +1,295 @@
+# mev-relay
+variable "mev_relay" {
+  default = {
+    name            = "mev-relay"
+    count           = 1
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Bootnode
+variable "bootnode" {
+  default = {
+    name            = "bootnode"
+    count           = 2
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Lighthouse
+variable "lighthouse_geth" {
+  default = {
+    name            = "lighthouse-geth"
+    count           = 5
+    validator_start = 0
+    validator_end   = 25000
+  }
+}
+
+variable "lighthouse_nethermind" {
+  default = {
+    name            = "lighthouse-nethermind"
+    count           = 5
+    validator_start = 25000
+    validator_end   = 50000
+  }
+}
+
+variable "lighthouse_besu" {
+  default = {
+    name            = "lighthouse-besu"
+    count           = 2
+    validator_start = 225000
+    validator_end   = 235000
+  }
+}
+
+variable "lighthouse_erigon" {
+  default = {
+    name            = "lighthouse-erigon"
+    count           = 2
+    validator_start = 235000
+    validator_end   = 245000
+  }
+}
+
+variable "lighthouse_ethereumjs" {
+  default = {
+    name            = "lighthouse-ethereumjs"
+    count           = 1
+    validator_start = 325000
+    validator_end   = 326000
+  }
+}
+
+# Prysm
+variable "prysm_geth" {
+  default = {
+    name            = "prysm-geth"
+    count           = 5
+    validator_start = 50000
+    validator_end   = 75000
+  }
+}
+
+variable "prysm_nethermind" {
+  default = {
+    name            = "prysm-nethermind"
+    count           = 5
+    validator_start = 75000
+    validator_end   = 100000
+  }
+}
+
+variable "prysm_besu" {
+  default = {
+    name            = "prysm-besu"
+    count           = 2
+    validator_start = 245000
+    validator_end   = 255000
+  }
+}
+
+variable "prysm_erigon" {
+  default = {
+    name            = "prysm-erigon"
+    count           = 2
+    validator_start = 255000
+    validator_end   = 265000
+  }
+}
+
+variable "prysm_ethereumjs" {
+  default = {
+    name            = "prysm-ethereumjs"
+    count           = 1
+    validator_start = 326000
+    validator_end   = 327000
+  }
+}
+
+# Lodestar
+variable "lodestar_geth" {
+  default = {
+    name            = "lodestar-geth"
+    count           = 5
+    validator_start = 100000
+    validator_end   = 125000
+  }
+}
+
+variable "lodestar_nethermind" {
+  default = {
+    name            = "lodestar-nethermind"
+    count           = 5
+    validator_start = 125000
+    validator_end   = 150000
+  }
+}
+
+variable "lodestar_besu" {
+  default = {
+    name            = "lodestar-besu"
+    count           = 2
+    validator_start = 265000
+    validator_end   = 275000
+  }
+}
+
+variable "lodestar_erigon" {
+  default = {
+    name            = "lodestar-erigon"
+    count           = 2
+    validator_start = 275000
+    validator_end   = 285000
+  }
+}
+
+variable "lodestar_ethereumjs" {
+  default = {
+    name            = "lodestar-ethereumjs"
+    count           = 1
+    validator_start = 327000
+    validator_end   = 328000
+  }
+}
+
+# Teku
+variable "teku_geth" {
+  default = {
+    name            = "teku-geth"
+    count           = 5
+    validator_start = 125000
+    validator_end   = 150000
+  }
+}
+
+variable "teku_nethermind" {
+  default = {
+    name            = "teku-nethermind"
+    count           = 5
+    validator_start = 150000
+    validator_end   = 175000
+  }
+}
+
+variable "teku_besu" {
+  default = {
+    name            = "teku-besu"
+    count           = 2
+    validator_start = 285000
+    validator_end   = 295000
+  }
+}
+
+variable "teku_erigon" {
+  default = {
+    name            = "teku-erigon"
+    count           = 2
+    validator_start = 295000
+    validator_end   = 305000
+  }
+}
+
+variable "teku_ethereumjs" {
+  default = {
+    name            = "teku-ethereumjs"
+    count           = 1
+    validator_start = 328000
+    validator_end   = 329000
+  }
+}
+
+# Nimbus
+variable "nimbus_geth" {
+  default = {
+    name            = "nimbus-geth"
+    count           = 5
+    validator_start = 175000
+    validator_end   = 200000
+  }
+}
+
+variable "nimbus_nethermind" {
+  default = {
+    name            = "nimbus-nethermind"
+    count           = 5
+    validator_start = 200000
+    validator_end   = 225000
+  }
+}
+
+variable "nimbus_besu" {
+  default = {
+    name            = "nimbus-besu"
+    count           = 2
+    validator_start = 305000
+    validator_end   = 315000
+  }
+}
+
+variable "nimbus_erigon" {
+  default = {
+    name            = "nimbus-erigon"
+    count           = 2
+    validator_start = 315000
+    validator_end   = 325000
+  }
+}
+
+variable "nimbus_ethereumjs" {
+  default = {
+    name            = "nimbus-ethereumjs"
+    count           = 1
+    validator_start = 329000
+    validator_end   = 330000
+  }
+}
+
+# Reth
+variable "lighthouse_reth" {
+  default = {
+    name            = "lighthouse-reth"
+    count           = 1
+    validator_start = 330000
+    validator_end   = 331000
+  }
+}
+
+variable "prysm_reth" {
+  default = {
+    name            = "prysm-reth"
+    count           = 1
+    validator_start = 331000
+    validator_end   = 332000
+  }
+}
+
+variable "lodestar_reth" {
+  default = {
+    name            = "lodestar-reth"
+    count           = 1
+    validator_start = 332000
+    validator_end   = 333000
+  }
+}
+
+variable "teku_reth" {
+  default = {
+    name            = "teku-reth"
+    count           = 1
+    validator_start = 333000
+    validator_end   = 334000
+  }
+}
+
+variable "nimbus_reth" {
+  default = {
+    name            = "nimbus-reth"
+    count           = 1
+    validator_start = 334000
+    validator_end   = 335000
+  }
+}
